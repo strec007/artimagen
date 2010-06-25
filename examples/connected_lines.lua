c63e4381a0c640e90e9b99e3af2cc77d17d3eed9
Petr Cizmar <petr@g.cizmar.org>
2010-06-25 12:02:44 -0400

    connected_lines.lua added to the examples directory
    Signed-off-by: Petr Cizmar <petr@g.cizmar.org>

---
+++ examples/connected_lines.lua
@@ -0,0 +1,97 @@
+-- This is a ARTIMAGEN Lua script that generates a connected set of vertical lines
+-- by Petr Cizmar @ NIST
+
+--[[
+    As this software was developed as part of work done by the United States
+    Government, it is not subject to copyright, and is in the public domain.
+    Note that according to GNU.org public domain is compatible with GPL.
+]]--
+
+math.randomseed(os.time()) -- Initialization of the random number generator with current time as seed
+
+imsize = 1024 -- Size of a side of the square image
+line_width = 30 -- Width of the defined lines
+line_pitch = 70 -- Pitch of the defined lines
+line_top = 70 -- Height of the beginnigns of the bends
+im = aig_new_image(imsize, imsize) -- Creation of a new empty
+
+background = {} -- New empty table for the background
+background_density = 8 -- background "map" will be 8x8 pixels large
+for i = 1, background_density*background_density, 1 do
+  table.insert(background, math.random()*(0.2-0.1) + 0.1) -- value for each pixel is randomly generated, value varies from 0.1 to 0.2
+end
+aig_apply_background_image(im, {background_density, background_density}, background) -- application of the background to the image
+
+edge_effect = aig_new_effect("edge", 0.2, 0.3) -- definition of the edge-effect
+fine_structure = aig_new_effect("finestructure", 70e-4, 6, 10, 0.95, 1.05) -- definition of the fine structure
+
+features = {} -- new empty table for the feature
+x1 = 0
+x = 0
+curves = {} -- new empty table for crves 
+-- POSITIVE X DIRECTION --
+while x1 < imsize do
+   x = x1
+   x1 = x + line_pitch + line_width
+   table.insert(curves, aig_new_curve("segment", {{x,line_top}, {x,imsize-line_top}})) -- definition of all curves
+   table.insert(curves, aig_new_curve("bezier", {{x,imsize-line_top}, {x,imsize}, {x1,imsize}, {x1,imsize-line_top}}))
+
+   x = x1
+   x1 = x + line_pitch - line_width
+   table.insert(curves, aig_new_curve("segment", {{x,imsize-line_top}, {x,line_top}}))
+   table.insert(curves, aig_new_curve("bezier", {{x,line_top}, {x,line_top/2}, {x1,line_top/2}, {x1,line_top}}))
+end
+
+-- END LINE--
+   x = x1
+   x1 = x + line_width
+   table.insert(curves, aig_new_curve("segment", {{x,line_top}, {x1,line_top}}))
+
+-- NEGATIVE X DIRECTION --
+while x > 0 do
+   x = x1
+   x1 = x - line_pitch - line_width
+   table.insert(curves, aig_new_curve("bezier", {{x,line_top}, {x,0}, {x1,0}, {x1,line_top}}))
+   table.insert(curves, aig_new_curve("segment", {{x1,line_top}, {x1,imsize-line_top}}))
+
+   x = x1
+   x1 = x - line_pitch + line_width
+   table.insert(curves, aig_new_curve("bezier", {{x,imsize-line_top}, {x,imsize-line_top/2}, {x1,imsize-line_top/2}, {x1,imsize-line_top}}))
+   table.insert(curves, aig_new_curve("segment", {{x1,imsize-line_top}, {x1,line_top}}))
+end
+
+-- END LINE --
+   x = x1
+   x1 = x - line_width
+   table.insert(curves, aig_new_curve("segment", {{x,line_top}, {x1,line_top}}))
+
+   lines_feature = aig_new_feature(curves, {edge_effect, fine_structure}, 0.3) -- composition of the curves and effect into a feature
+   table.insert(features, lines_feature); -- insertion of the new feature into the table
+
+lines_sample = aig_new_sample(imsize, imsize, features) -- creation if the sample
+aig_paint_sample(im, lines_sample) -- painting of the sample to the image
+aig_delete_sample(lines_sample) -- sample is no more needed, thus it should be deleted
+aig_apply_gaussian_psf(im, 0.5,1,30) -- application of the Gaussian blur
+
+-- definition of the drift/vibration.
+freqs = 8 -- the vib. function will be composed of 8 sine functions
+ampl=0.2 -- amplitude on each function is 0.2 pixels
+freq=100 -- maximum frequency is 100, minimum is 0
+vibs={} -- empty table for sine functions
+for i = 1, freqs, 1 do
+  vibs[i]={math.random()*(ampl), math.random()*(ampl), math.random()*(freq), math.random()*(2*math.pi)} -- random generation of parameters
+end
+
+aig_apply_vib(im,1000,50,100,0,vibs) -- application of vibrations
+aig_apply_noise(im, "poisson", 20) -- application of Poisson noise
+--aig_apply_noise(im, "gaussian", 0.01)
+
+--[[ UNCOMMENT IF YOU WANT PREVIEW
+previm = aig_copy_image(im) -- copy of the original large image
+aig_crop_image(previm,0,0,512,512) -- crop it to 512x512 (preview requierement)
+gaig_preview(previm) -- gAIG specific - preview. If running with artimagenl, comment this line out.
+aig_delete_image(previm) -- deletion of the preview image; it is no more needed
+]]--
+
+aig_save_image(im, "connected_lines.tiff","Connected Lines, by Petr Cizmar") --saving of the image to a file
+aig_delete_image(im) -- deletion of the image
